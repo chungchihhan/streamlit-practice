b1db3e6a15f80e2fe4888914067e4a7c5082cc65
chungchihhan <harryup2000@gmail.com>
2024-04-18 14:26:18 +0800

    Web cralwer RAG

---
+++ langchain-faiss-RAG-streaming-Crawler.py
@@ -0,0 +1,72 @@
+import bs4
+from langchain import hub
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+import streamlit as st
+
+st.title("RAG!web crawler")
+# Load, chunk and index the contents of the blog.
+bs_strainer = bs4.SoupStrainer(class_=("post-content", "post-title", "post-header"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs_strainer},
+)
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+# Retrieve and generate using the relevant snippets of the blog.
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain_from_docs = (
+    RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+rag_chain_with_source = RunnableParallel(
+    {"context": retriever, "question": RunnablePassthrough()}
+).assign(answer=rag_chain_from_docs)
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+def response_generator(prompt):
+    # Start the stream from the RAG chain with the provided prompt
+    stream = rag_chain_with_source.stream(prompt)
+    for output in stream:
+        if 'answer' in output and output['answer']:
+            # Assuming output['answer'] is a string, split and yield each word
+            for word in output['answer'].split():
+                yield word + " "
+
+if prompt := st.chat_input("What is up?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator(prompt= prompt))
+    st.session_state.messages.append({"role": "assistant", "content": response})
